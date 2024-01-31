dd53be903ff5a3f74022864289c6572c98630cbe
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-01-31 13:47:25 -0300

    add dbh_classes_generate function

--- modules/dbh_classes_generate.R
+++ modules/dbh_classes_generate.R
@@ -1,11 +1,12 @@
-#' dbh_class function
+#' @name dbh_class function
 #' 
 #' @description 
 #' This function takes any data frame from a global forest inventory with DBH of 
 #' trees and returns a new data frame containing the variable class (class of DBH)
 #' for each tree.
 #' 
-#' @param dataset
+#' @param df Data frame with DBH column (dap).
+#' 
 #' 
 #' 
 
@@ -14,8 +15,8 @@ library(magrittr)
 
 
 # Set the function
-dbh_class <- function(df) {
-        
+dbh_classes_generate <- function(df) {
+        #browser()
         limits <- seq(10, 1000, 10)
         labels <- paste0(limits[-length(limits)], "-", limits[-1])
         labels2 <- append(labels, ">200")
@@ -28,12 +29,12 @@ dbh_class <- function(df) {
         # Set class2
         df %<>%
                 mutate(classe2 = if_else(dap > 200, ">200", as.character(classe))) %>%
-                mutate(classe2 = factor(classe2, levels = labels2, ordered = TRUE)) %>%
-                select(num_arvore, flona, umf, upa, ut, nome_cientifico, familia,
-                       status, dispositivo_legal, nome_popular, cap, dap, classe,
-                       classe2, altura, qf, g, volume, categoria, lat, lon, norte,
-                       leste, zona_utm, mc, typo, isSynonym, nome_aceito, familia,
-                       status_conservacao, dispositivo_legal, status, aem)
+                mutate(classe2 = factor(classe2, levels = labels2, ordered = TRUE))
+                # select(num_arvore, flona, umf, upa, ut, nome_cientifico, familia,
+                #        status, dispositivo_legal, nome_popular, cap, dap, classe,
+                #        classe2, altura, qf, g, volume, categoria, lat, lon, norte,
+                #        leste, zona_utm, mc, typo, isSynonym, nome_aceito, familia,
+                #        status_conservacao, dispositivo_legal, status, aem)
         
         # Assign the local copy of the data frame to the R global environment
         #df <<- df
