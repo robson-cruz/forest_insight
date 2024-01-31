f61bf26cc3fd8d8237273727815caeb50027123f
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-01-31 16:45:41 -0300

    Set 'update function

--- modules/dbh_classes_generate.R
+++ modules/dbh_classes_generate.R
@@ -16,20 +16,19 @@ library(magrittr)
 
 # Set the function
 dbh_classes_generate <- function(df) {
-        #browser()
+        # Set breaks and labels for DBH classes
         limits <- seq(10, 1000, 10)
         labels <- paste0(limits[-length(limits)], "-", limits[-1])
         labels2 <- append(labels, ">200")
         
+        # Create a new column 'classe' based on DBH breaks
         df$classe <- cut(df$dap, breaks = limits, labels = labels, right = FALSE)
-        
-        # Set class column as factor
+        # Set 'classe' column as factor
         df$classe <- factor(df$classe, levels = labels, ordered = TRUE)
-        
-        # Set class2
-        df %<>%
-                mutate(classe2 = if_else(dap > 200, ">200", as.character(classe))) %>%
-                mutate(classe2 = factor(classe2, levels = labels2, ordered = TRUE))
+
+        # Create a new column 'classe2' based on DBH breaks (with >200 category)
+        df$classe2 <- ifelse(df$dap > 200, ">200", as.character(df$classe))
+        df$classe2 <- factor(df$classe2, levels = labels2, ordered = TRUE)
                 # select(num_arvore, flona, umf, upa, ut, nome_cientifico, familia,
                 #        status, dispositivo_legal, nome_popular, cap, dap, classe,
                 #        classe2, altura, qf, g, volume, categoria, lat, lon, norte,
@@ -37,6 +36,5 @@ dbh_classes_generate <- function(df) {
                 #        status_conservacao, dispositivo_legal, status, aem)
         
         # Assign the local copy of the data frame to the R global environment
-        #df <<- df
         assign('df', df, envir = .GlobalEnv)
 }
