e84dd1e277cb1bc56722caa31f6cb16d1cd6e554
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-01-31 13:57:57 -0300

    Add tabPnels

--- ui.R
+++ ui.R
@@ -5,20 +5,19 @@ shinyUI(
         fluidPage(
                 navbarPage(
                         title = 'Forest Insight v1.0.0 - Apuleia',
-                        fluidRow(
-                                tabPanel(
-                                        "Carregar Arquivo",
-                                        fileInput("uploadFile", "Selecione a planilha"),
-                                        actionButton("loadFile", "Carregar arquivo"),
-                                        textOutput("fileStatus")
-                                )
+                        lang = "pt-BR",
+                        tabPanel(
+                                "Carregar Arquivo",
+                                fileInput("uploadFile", "Selecione a planilha"),
+                                actionButton("loadFile", "Carregar arquivo"),
+                                textOutput("fileStatus")
                         ),
-                        fluidRow(
-                                tabPanel(
-                                        "Ver Dados!",
-                                        DT::dataTableOutput('tbl')
-                                )
-                        )
+                        tabPanel("Ver Dados", DT::dataTableOutput('tbl')),
+                        tabPanel("Baixar Análise", downloadLink('DownloadDataAnalysis', 'Download')),
+                        tabPanel("Normas Florestais", a(href = 'https://github.com/rcflorestal/endangeredBrazilianPlantSpecies/blob/main/README.md')),
+                        tabPanel('Planilha Modelo', downloadLink('DownloadData', 'Download')),
+                        
+                        
                 )
         )
 )
