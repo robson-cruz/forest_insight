38ab1e4196ebd2d62641d18d2f4220876fbe6657
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-02-01 11:27:16 -0300

    Add css link

--- ui.R
+++ ui.R
@@ -1,8 +1,13 @@
 # ui.R
 library(shiny)
 
+
 shinyUI(
         fluidPage(
+                tags$head(
+                        tags$style(type = "text/css", includeCSS("./www/css/styles.css"))
+                        
+                ),
                 navbarPage(
                         title = 'Forest Insight v1.0.0 - Apuleia',
                         lang = "pt-BR",
@@ -13,11 +18,13 @@ shinyUI(
                                 textOutput("fileStatus")
                         ),
                         tabPanel("Ver Dados", DT::dataTableOutput('tbl')),
+                        tabPanel('Gráficos', plotOutput('DBH_classes_plot')),
                         tabPanel("Baixar Análise", downloadLink('DownloadDataAnalysis', 'Download')),
-                        tabPanel("Normas Florestais", a(href = 'https://github.com/rcflorestal/endangeredBrazilianPlantSpecies/blob/main/README.md')),
-                        tabPanel('Planilha Modelo', downloadLink('DownloadData', 'Download')),
-                        
-                        
+                        tabPanel(
+                                "Normas Florestais",
+                                HTML("<a href = 'https://www.ibama.gov.br/sophia/cnia/livros/normasflorestaisfederaisparaaamazonia.pdf' target = '_blank'>Acessar Normas Florestais</a>")
+                        ),
+                        tabPanel('Planilha Modelo', downloadLink('DownloadData', 'Download'))
                 )
         )
 )
