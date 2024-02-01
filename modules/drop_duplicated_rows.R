f62d51175b1722b19ffe64ca46708bd098bf695d
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-02-01 11:25:42 -0300

    Add Drop Duplicated Rows function

--- modules/drop_duplicated_rows.R
+++ modules/drop_duplicated_rows.R
@@ -1,31 +1,25 @@
-#' drop_duplicated function
-#' 
+#' @name drop_duplicated_rows
+#'
+#' @title Drop Duplicated Rows
+#'  
 #' @description 
 #' This function takes any data frame from a global forest inventory with duplica- 
 #' ted rows returns a new data frame containing only unique rows as also save the 
 #' duplicated rows as .csv file in a folder called 'alert'.
 #' 
-#' @param dataset
+#' @param data frame
 #' 
 #' 
-
-# Set function to remove duplicates rows
-drop_duplicated <- function(df) {
+drop_duplicated_rows <- function(df) {
         
         # Check out for duplicated rows
         if (sum(duplicated(df$num_arvore)) > 0) {
                 
                 # Save the duplicated row
                 d <- df[duplicated(df$num_arvore), ]
-                if (!dir.exists('./output/warnings')) {
-                        dir.create('./output/warnings')
-                        write.csv2(d, './output/warnings/duplicated_rows.csv',
-                                   row.names = FALSE)
-                } else {
-                        write.csv2(d, './output/warnings/duplicated_rows.csv',
-                                   row.names = FALSE)
-                }
-                
+                write.csv2(d, './output/Pendencias/Arvores_Placa_Duplicada.csv',
+                           fileEncoding = "latin1",
+                           row.names = FALSE)
                 
                 # Drop Partial Duplicates
                 df <- df %>%
@@ -34,7 +28,5 @@ drop_duplicated <- function(df) {
                 # Assign the local copy of df to the global R environment
                 assign('df', df, envir = .GlobalEnv)
                 
-        } else {
-                df
         }
 }
