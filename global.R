88d264fa12e3044c400c30d995b42767a2d774ab
Robson Cruz <rcflorestal@yahoo.com.br>
2024-02-02 22:54:26 -0300

    add 'aem' dataset from user

--- global.R
+++ global.R
@@ -8,12 +8,20 @@ library(magrittr, warn.conflicts = FALSE)
 
 
 # Load data from the app
-user_data <- NULL
+aem <- NULL
+
+setUploadAem <- function(data) {
+        aem <<- data
+        
+        assign("aem", aem, inherits = TRUE, envir = .GlobalEnv)
+}
+
+inventario <- NULL
 
 setUploadedData <- function(data) {
-        user_data <<- data
+        inventario <<- data
         
-        user_data %<>%
+        inventario %<>%
                 mutate(categoria = R.utils::capitalize(categoria)) %>%
                 # Diameter at Breast Height - DBH ("dap" in Portuguese)
                 mutate(dap = cap / pi) %>%
@@ -22,7 +30,9 @@ setUploadedData <- function(data) {
                 # Geometric volume
                 mutate(vol_geo = g * altura * 0.7) %>%
                 # Define 'qf' column as a factor
-                mutate(qf = factor(qf, levels = c(1L, 2L, 3L)))
+                mutate(qf = factor(qf, levels = c(1L, 2L, 3L))) %>%
+                # Define 'Categoria2' column
+                mutate(categoria2 = if_else(categoria == "Substituta", "Remanescente", categoria))
         
-        assign("user_data", user_data, inherits = TRUE, envir = .GlobalEnv)
+        assign("inventario", inventario, inherits = TRUE, envir = .GlobalEnv)
 }
