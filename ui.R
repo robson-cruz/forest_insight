737f21fe33cde8865965031430032851b05380dc
Robson Cruz <rcflorestal@yahoo.com.br>
2024-02-02 22:55:42 -0300

    Changed the UI to fluidPage to navbarPage

--- ui.R
+++ ui.R
@@ -1,30 +1,96 @@
-# ui.R
 library(shiny)
+library(shinydashboard)
 
 
 shinyUI(
-        fluidPage(
-                tags$head(
-                        tags$style(type = "text/css", includeCSS("./www/css/styles.css"))
-                        
-                ),
-                navbarPage(
-                        title = 'Forest Insight v1.0.0 - Apuleia',
-                        lang = "pt-BR",
-                        tabPanel(
-                                "Carregar Arquivo",
-                                fileInput("uploadFile", "Selecione a planilha"),
-                                actionButton("loadFile", "Rodar Análise"),
-                                textOutput("fileStatus")
-                        ),
-                        tabPanel("Ver Dados", DT::dataTableOutput('tbl')),
-                        tabPanel('Gráficos', plotOutput('DBH_classes_plot')),
-                        tabPanel("Baixar Análise", downloadLink('DownloadDataAnalysis', 'Download')),
-                        tabPanel(
-                                "Normas Florestais",
-                                HTML("<a href = 'https://www.ibama.gov.br/sophia/cnia/livros/normasflorestaisfederaisparaaamazonia.pdf' target = '_blank'>Acessar Normas Florestais</a>")
-                        ),
-                        tabPanel('Planilha Modelo', downloadLink('DownloadData', 'Download'))
-                )
-        )
+    navbarPage("Forest Insight v1.0.0 - Apuleia",
+               tags$head(
+                   tags$style(type = "text/css", includeCSS("./www/css/styles.css"))
+                   
+               ),
+               tabPanel(
+                   "Planilha Áreas de Efetivo Manejo",
+                   fileInput(inputId = "uploadAem", label = "Planilha Áreas de Efetivo Manejo"),
+                   actionButton(inputId = "uploadFileAem", label = "Fazer upload"),
+                   textOutput("fileStatusAem")
+               ),
+               tabPanel(
+                   "Planilha do Inventário",
+                   fileInput(inputId = "uploadInventario", label = "Planilha Inventário"),
+                   actionButton(inputId = "uploadFileInventario", "Fazer upload e Rodar Análise"),
+                   textOutput("fileStatusInventario")
+               ),
+               navbarMenu(
+                   "Planilha Modelo",
+                   tabPanel(
+                       "Planilha Modelo", 
+                       downloadButton(outputId = "DownloadDataModel", label = "Baixar Análise")
+                   )
+               ),
+               navbarMenu(
+                   "Normas Florestais",
+                   HTML("<a href = 'https://www.ibama.gov.br/sophia/cnia/livros/normasflorestaisfederaisparaaamazonia.pdf' target = '_blank'>Acessar Normas Florestais</a>")
+               ),
+               navbarMenu(
+                   "Ver Análise",
+                   tabPanel(
+                       "Gráficos",
+                       sidebarPanel(
+                           radioButtons(inputId = "plotType", label = "Gráfico",
+                                        choices = c("Distribuição Diamétrica" = "dap",
+                                                    "Distribuição Diamétrica por UT" = "dapUt",
+                                                    "Área Basal por Classe de DAP" = "areaBasalDap",
+                                                    "Área Basal por UT" = "areaBasalUt",
+                                                    "Seleção por Classe de DAP" = "dapCorte",
+                                                    "Qualidade de Fuste" = "qf",
+                                                    "Seleção por Categoria" = "categorias",
+                                                    "Critério 10% a 15%" = "crit1015")
+                           )
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'dapUt'",
+                           plotOutput("")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'dap'",
+                           plotOutput("DBH_classes_plot")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'areaBasalDap'",
+                           plotOutput("")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'areaBasalUt'",
+                           plotOutput("")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'dapCorte'",
+                           plotOutput("")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'qf'",
+                           plotOutput("qf_plot")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'categorias'",
+                           plotOutput("")
+                       ),
+                       conditionalPanel(
+                           condition = "input.plotType == 'crit1015'",
+                           plotOutput("")
+                       )
+                   ),
+                   tabPanel(
+                       "Inventário Processado",
+                       DT::dataTableOutput(outputId = "verDados")
+                   ),
+                   tabPanel("Resumo Inventário",
+                            verbatimTextOutput(outputId = "summary")
+                   ),
+                   tabPanel(
+                       "Baixar Análise Completa",
+                       downloadLink('DownloadDataAnalysis', 'Download')
+                   )
+               )
+    )
 )
