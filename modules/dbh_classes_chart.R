ea9db14922dc83882d1bcf3cbf198dc7400186ae
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2024-02-05 15:34:52 -0300

    Solved issue graphics not saved on the user's disk. (#4)

--- modules/dbh_classes_chart.R
+++ modules/dbh_classes_chart.R
@@ -11,38 +11,47 @@
 #' 
 #' @return A ggplot object representing the DBH classes distribution of trees.
 #'
-dbh_classes_chart <- function(df, threshold = 40) {
-        
-        # Filter data
-        df_filtered <- df[df$dap >= threshold, ] %>%
-                group_by(classe2) %>%
-                summarize(N = n())
-        
-        # Create ggplot and save as an image
-        png(
-                paste0('./output/Graficos/Distribuicao_Diametrica/',
-                       'Distribuicao_Diametrica', '.png'),
-                width = 1500,
-                height = 950,
-                res = 300
-        )
-        DBH_classes_plot <- ggplot(df_filtered, aes(x = classe2, y = N)) +
-                geom_bar(stat = 'identity', fill = 'steelblue') +
-                scale_y_continuous(labels = scales::label_number(big.mark = '.')) +
-                theme(
-                        axis.text.x = element_text(angle = 50, size = 7),
-                        axis.text.y = element_text(size = 8),
-                        axis.title.x = element_text(size = 8),
-                        axis.title.y = element_text(size = 9),
-                        plot.title = element_text(hjust = 0.5, size = 10)
-                ) +
-                labs(
-                        title = 'Distribuição Diamétrica',
-                        x = 'Classes de Diâmetro (cm)',
-                        y = 'Número de Árvores'
-                )
-        print(DBH_classes_plot)
-        dev.off()
+library(dplyr)
+library(ggplot2)
 
-        assign('DBH_classes_plot', 'DBH_classes_plot', envir = .GlobalEnv)
+
+dbh_classes_chart <- function(df, threshold = 40) {
+    output_dir <- './output/Graficos/Distribuicao_Diametrica/'
+    if (!dir.exists(output_dir)) {
+        dir.create(output_dir, recursive = TRUE)
+    }
+    
+    # Filter data
+    df_filtered <- df[df$dap >= threshold, ] %>%
+            group_by(classe2) %>%
+            summarize(N = n())
+        
+    # Create ggplot and save as an image
+    png(
+            paste0(output_dir,
+                   'Distribuicao_Dap', '.png'),
+            width = 1500,
+            height = 950,
+            res = 300
+    )
+    DBH_classes_plot <- ggplot(df_filtered, aes(x = classe2, y = N)) +
+            geom_bar(stat = 'identity', fill = 'steelblue') +
+            scale_y_continuous(labels = scales::label_number(big.mark = '.')) +
+            theme(
+                    axis.text.x = element_text(angle = 50, size = 7),
+                    axis.text.y = element_text(size = 8),
+                    axis.title.x = element_text(size = 8),
+                    axis.title.y = element_text(size = 9),
+                    plot.title = element_text(hjust = 0.5, size = 10)
+            ) +
+            labs(
+                    title = 'Distribuição Diamétrica',
+                    x = 'Classes de Diâmetro (cm)',
+                    y = 'Número de Árvores'
+            )
+    
+    print(DBH_classes_plot)
+
+    dev.off()
+
 }
