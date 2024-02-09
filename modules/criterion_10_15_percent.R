57ebc613c2cc205730059d274fa1effd49d24eab
rcDeveloping <rcflorestal@yahoo.com.br>
2024-02-09 16:43:02 -0300

    Solved factor issue and added some aesthetics (#9)

--- modules/criterion_10_15_percent.R
+++ modules/criterion_10_15_percent.R
@@ -8,8 +8,8 @@
 #' trees by species and whether or not they meet the maintenance criterion from
 #' 10% to 15 % of remaining trees by species in the annual production area.
 #' 
-#' @param dataframe - Forest inventory data frame with columns nome_cientifico,
-#' categoria2, status and aem.
+#' @param dataframe - Forest inventory data frame with columns `nome_cientifico`,
+#' `categoria2`, `status` and `aem`.
 #' 
 #' @return A ggplot object and a dataframe with the percentage of remaining trees
 #' by species and whether or not they meet the maintenance criterion from 10% to
@@ -24,53 +24,59 @@ library(ggplot2)
 
 
 criterion_1015 <- function(data_frame) {
-crit_10.15 <- data_frame %>%
-            filter(dap >= 50 & as.numeric(qf) <= 2) %>%
-            select(nome_cientifico, categoria2, status, aem) %>%
-            group_by(nome_cientifico) %>%
-            mutate(
-                Corte = sum(categoria2 == 'Explorar'),
-                Remanescente = sum(categoria2 == 'Remanescente'),
-                Total = Corte + Remanescente,
-                PercRem = round(Remanescente / Total * 100),
-                Criterio = if_else(status == 'Não Ameaçada', (10), (15)),
-                Analise = if_else(PercRem >= Criterio | Corte == 0, 'Atende', 'Nao Atende')
-            ) %>%
-            # By default, mutate() keeps all columns from the input data
-            # Use '.keep_all' to override it
-            distinct(nome_cientifico, .keep_all = TRUE) %>%
-            select(-c(aem, categoria2)) %>%
-            filter(Corte != 0) %>%
-            arrange(nome_cientifico)
+    crit_10.15 <- data_frame %>%
+        filter(dap >= 50 & as.numeric(as.factor(qf)) <= 2) %>%
+        select(nome_cientifico, categoria2, status, aem) %>%
+        group_by(nome_cientifico) %>%
+        mutate(
+            Corte = sum(categoria2 == 'Explorar'),
+            Remanescente = sum(categoria2 == 'Remanescente'),
+            Total = Corte + Remanescente,
+            PercRem = round(Remanescente / Total * 100),
+        ) %>%
+        mutate(
+            Criterio = if_else(status == 'Não Ameaçada', 10, 15),
+            Analise = if_else(PercRem >= Criterio |
+                                  Corte == 0, 'Atende', 'Nao Atende')
+        ) %>%
+        # By default, mutate() keeps all columns from the input data
+        # Use '.keep_all' to override it
+        distinct(nome_cientifico, .keep_all = TRUE) %>%
+        select(-c(aem, categoria2)) %>%
+        filter(Corte != 0) %>%
+        arrange(nome_cientifico)
     
     output_dir <- './output/Planilhas/'
     if (!dir.exists(output_dir)) {
         dir.create(output_dir, recursive = TRUE)
     }
     
-    file_name_crit1015 <- paste0(
-        output_dir, 'Criterio_10-15_Porcento', '.csv'
-    )
-
-    file_name_crit1015_not <- paste0(
-            './output/Pendencias/', 'Criterio_10-15_Porcento', '.csv'
-    )
-
-    if (nrow(crit_10.15[crit_10.15$Analise == 'Nao Atende', ])) {
-        write.csv2(crit_10.15[crit_10.15$Analise == 'Nao Atende', ],
-                   file = file_name_crit1015_not,
-                   row.names = FALSE,
-                   na = 'NA',
-                   fileEncoding = 'latin1')
+    file_name_crit1015 <- paste0(output_dir, 'Criterio_10-15_Porcento', '.csv')
+    
+    file_name_crit1015_not <- paste0('./output/Pendencias/',
+                                     'Criterio_10-15_Porcento',
+                                     '.csv')
+    
+    not <- crit_10.15[crit_10.15$Analise == 'Nao Atende', ]
+    
+    if (nrow(not)) {
+        write.csv2(
+            not,
+            file = file_name_crit1015_not,
+            row.names = FALSE,
+            na = 'NA',
+            fileEncoding = 'latin1'
+        )
+    } else {
+        write.csv2(
+            crit_10.15,
+            file = file_name_crit1015,
+            row.names = FALSE,
+            na = 'NA',
+            fileEncoding = 'latin1'
+        )
     }
-
-    write.csv2(crit_10.15,
-               file = file_name_crit1015,
-               row.names = FALSE,
-               na = 'NA',
-               fileEncoding = 'latin1'
-    )
-
+    
     
     # Lollipop Chart
     output_dir <- './output/Graficos/Criterio_10_a_15_Porcento/'
@@ -78,56 +84,95 @@ crit_10.15 <- data_frame %>%
         dir.create(output_dir, recursive = TRUE)
     }
     
-    png(paste0(output_dir, 'Criterio_10_a_15_Porcento', '.png'),
+    png(
+        paste0(output_dir, 'Criterio_10_a_15_Porcento', '.png'),
         width = 1500,
         height = 1500,
-        res = 300)
+        res = 300
+    )
     
-    crit_10.15_plt <- crit_10.15 %>%
-            rename(Análise = Analise) %>%
-            mutate(
-                nome_cientifico = ifelse(!status == 'Não Ameaçada',
-                                         paste(nome_cientifico, '*'),
-                                         nome_cientifico)
-            ) %>%
-            ggplot(aes(x = PercRem, y = nome_cientifico)) +
-            geom_point(size = 1, color = 'tomato') +
-            geom_segment(
-                aes(x = Criterio,
-                    xend = PercRem,
-                    y = nome_cientifico,
-                    yend = nome_cientifico),
-                color = 'steelblue'
-            ) +
-            geom_text(
-                color = 'purple',
-                size = 2,
-                hjust = -0.25,
-                aes(label = paste0(PercRem, '%'))
-            ) +
-            scale_x_continuous(breaks =  seq(0, 100, 5)) +
-            labs(
-                x = '',
-                y = NULL,
-                title = 'Percentual de Árvores Remanescentes',
-                caption = '* Espécie Vulnerável'
-            ) +
-            theme(
-                plot.title = element_text(size = 10, hjust = 0.5),
-                plot.background = element_rect(fill = '#f7f7f7'),
-                panel.background = element_rect(fill = '#D8D8D8'), #f7f7f7
-                panel.grid.minor = element_blank(),
-                #panel.grid.major.y = element_blank(),
-                panel.grid.major.x = element_line(),
-                axis.ticks.x = element_blank(),
-                axis.text.x = element_blank(),
-                axis.text.y = element_text(size = 8, face = 'italic'),
-                plot.caption = element_text(size = 6, face = 'italic'),
-                legend.position = 'top',
-                panel.border = element_blank()
-            )
+    data2plt <- crit_10.15 %>%
+        rename(Análise = Analise) %>%
+        mutate(
+            nome_cientifico = ifelse(!status == 'Não Ameaçada',
+                                     paste(nome_cientifico, '*'),
+                                     nome_cientifico)
+        ) %>%
+        mutate(nome_cientifico = factor(nome_cientifico, nome_cientifico)) %>%
+        mutate(nome_cientifico = forcats::fct_reorder(nome_cientifico, Criterio))
+    
+    crit_10.15_plt <- ggplot(data2plt, aes(x = nome_cientifico, y = Criterio)) +
+        
+        geom_segment(
+            aes(x = nome_cientifico, xend = nome_cientifico, y = Criterio, yend = PercRem),
+            color = 'steelblue', size = 0.3
+        ) +
+        
+        geom_point(aes(x = nome_cientifico, y = Criterio),
+                   color = '#597E52',
+                   size = 1,
+                   shape = 21,
+                   fill = '#597E52') +
+        
+        geom_point(aes(x = nome_cientifico, y = PercRem),
+                   color = '#5D3587',
+                   size = 1,
+                   shape = 21,
+                   fill = '#5D3587') +
+        
+        geom_text(
+            aes(x = nome_cientifico, y = PercRem, label = paste0(PercRem, '%')),
+            color = '#5D3587',
+            size = 1.6,
+            hjust = ifelse(data2plt$PercRem <= min(data2plt$PercRem), 1.6, -0.2),
+            nudge_y = 1
+        ) +
+        
+        coord_flip() +
+        
+        scale_y_continuous(breaks =  seq(10, 100, 5), labels = scales::label_percent(scale = 1)) +
+        
+        # annotate(
+        #     geom = "rect",
+        #     aes(data2plt),
+        #     ymin = ,
+        #     ymax = ,
+        #     xmin = ,
+        #     xmax = ,
+        #     alpha = 0.2, color = "blue", fill = "blue"
+        # ) +
+        
+        labs(
+            x = NULL,
+            y = NULL,
+            title = 'Percentual de Árvores Remanescentes',
+            caption = '* Espécie Vulnerável',
+            color = 'Análise: '
+        ) +
+        
+        theme(
+            plot.title = element_text(size = 10, hjust = 0.5),
+            #panel.background = element_rect(fill = '#f7f7f7'),
+            panel.background = element_blank(),
+            #panel.grid.minor = element_blank(),
+            #panel.grid.major.y = element_blank(),
+            #panel.grid.major.x = element_blank(),
+            #axis.ticks.x = element_blank(),
+            #axis.ticks.y = element_line(linewidth = 0.4),
+            #axis.ticks.x = element_line(linewidth = 0.4),
+            axis.ticks.y = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.text.x = element_text(size = 5),
+            axis.text.y = element_text(size = 6, face = 'italic'),
+            plot.caption = element_text(size = 6, face = 'italic'),
+            legend.position = 'bottom',
+            legend.title = element_text(size = 7),
+            legend.text = element_text(size = 7),
+            legend.background = element_rect(color = '#f7f7f7'),
+            legend.key = element_blank()
+        )
     
     print(crit_10.15_plt)
     dev.off()
-        
+    
 }
