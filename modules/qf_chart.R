1a7c5d68901b85cb0296dfee6b9db714ae571711
sghubdev <124167115+sghubdev@users.noreply.github.com>
2024-02-15 15:52:39 -0300

    Update qf_chart.R
    Error in `geom_bar()`:
    ! Problem while computing aesthetics.
    ℹ Error occurred in the 1st layer.
    Caused by error in `forcats::fct_reorder()`:
    ! `.f` must be a factor or character vector, not a double vector.
    
    Signed-off-by: sghubdev <124167115+sghubdev@users.noreply.github.com>

--- modules/qf_chart.R
+++ modules/qf_chart.R
@@ -11,36 +11,38 @@
 #' @return a ggplot object
 #' 
 qf_chart <- function(df) {
-        png(
-                paste0('./output/Graficos/Qualidade_de_Fuste/', 'Qualidade_de_Fuste', '.png'),
-                width = 1500,
-                height = 950,
-                res = 300
+    png(
+        paste0('./output/Graficos/Qualidade_de_Fuste/', 'Qualidade_de_Fuste', '.png'),
+        width = 1500,
+        height = 950,
+        res = 300
+    )
+    
+    data2plt <- df %>%
+        mutate(categoria2 = as.character(as.factor(categoria2)))
+    
+    qf_plot <- ggplot(data2plt, aes(forcats::fct_reorder(qf, categoria2), fill = categoria2)) +
+        geom_bar(
+            position = position_dodge2(preserve = 'total'),
+            alpha = 0.75,
+            width = 0.70
+        ) +
+        theme(
+            #axis.text.x = element_text(angle = 50, size = 7),
+            axis.text.y = element_text(size = 8),
+            axis.title.x = element_text(size = 8),
+            axis.title.y = element_text(size = 9),
+            plot.title = element_text(hjust = 0.5, size = 10),
+            legend.position = 'bottom'
+        ) +
+        labs(
+            title = 'Qualidade de Fuste e Seleção para Corte',
+            x = 'Qualidade de Fuste',
+            y = 'Número de Árvores',
+            fill = 'Seleção'
         )
-        
-        qf_plot <- ggplot(df, aes(forcats::fct_reorder(qf, categoria2), fill = categoria2)) +
-                geom_bar(
-                        position = position_dodge2(preserve = 'total'),
-                        alpha = 0.75,
-                        width = 0.70
-                ) +
-                theme(
-                        #axis.text.x = element_text(angle = 50, size = 7),
-                        axis.text.y = element_text(size = 8),
-                        axis.title.x = element_text(size = 8),
-                        axis.title.y = element_text(size = 9),
-                        plot.title = element_text(hjust = 0.5, size = 10),
-                        legend.position = 'bottom'
-                ) +
-                labs(
-                        title = 'Qualidade de Fuste e Seleção para Corte',
-                        x = 'Qualidade de Fuste',
-                        y = 'Número de Árvores',
-                        fill = 'Seleção'
-                )
-        
-        # Close the graphics device and assign the object to the environment
-        print(qf_plot)
-        dev.off()
+    
+    # Close the graphics device and assign the object to the environment
+    print(qf_plot)
+    dev.off()
 }
-
