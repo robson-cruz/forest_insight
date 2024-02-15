78b46bff3980ed4d078b097ca13edbfb05c4dcec
rcDeveloping <rcflorestal@yahoo.com.br>
2024-02-15 15:09:17 -0300

    Define 'categoria2' column as a factor

--- global.R
+++ global.R
@@ -19,20 +19,21 @@ setUploadAem <- function(data) {
 inventario <- NULL
 
 setUploadedData <- function(data) {
-        inventario <<- data
+    inventario <<- data
         
-        inventario %<>%
-                mutate(categoria = R.utils::capitalize(categoria)) %>%
-                # Diameter at Breast Height - DBH ("dap" in Portuguese)
-                mutate(dap = cap / pi) %>%
-                # Basal area in square meters
-                mutate(g = pi * dap**2 / 40000) %>%
-                # Geometric volume
-                mutate(vol_geo = g * altura * 0.7) %>%
-                # Define 'qf' column as a factor
-                mutate(qf = factor(qf, levels = unique(qf))) %>%
-                # Define 'Categoria2' column
-                mutate(categoria2 = if_else(categoria == "Substituta", "Remanescente", categoria))
-        
-        assign("inventario", inventario, inherits = TRUE, envir = .GlobalEnv)
+    inventario %<>%
+        mutate(categoria = R.utils::capitalize(categoria)) %>%
+        # Diameter at Breast Height - DBH ("dap" in Portuguese)
+        mutate(dap = cap / pi) %>%
+        # Basal area in square meters
+        mutate(g = pi * dap**2 / 40000) %>%
+        # Geometric volume
+        mutate(vol_geo = g * altura * 0.7) %>%
+        # Define 'qf' column as a factor
+        mutate(qf = factor(qf, levels = unique(qf))) %>%
+        # Define 'Categoria2' column
+        mutate(categoria2 = if_else(categoria == "Substituta", "Remanescente", categoria)) %>%
+        mutate(categoria2 = factor(categoria2, levels = unique(categoria2)))
+    
+    assign("inventario", inventario, inherits = TRUE, envir = .GlobalEnv)
 }
