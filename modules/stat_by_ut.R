f8df8b456db817b0c60932e0039a798eed79faa7
sghubdev <slavegithub@gmail.com>
2024-02-16 13:36:07 -0300

    Change modules parameter from 'df' to 'dataframe'

--- modules/stat_by_ut.R
+++ modules/stat_by_ut.R
@@ -1,27 +1,27 @@
 #' @name stat_by_ut
-#' 
+#'
 #' @title Descriptive statistics by `ut`
-#' 
+#'
 #' @description This function receives a data frame with the columns `ut`, `dap`, `g`,
 #' `altura`, and `vol_geo` and makes descriptive statistics of these columns
 #' grouped by the "ut" column.
-#' 
+#'
 #' @param dataframe - Forest inventory data frame with columns `ut`, `dap`, `g`,
 #' `altura`, and `vol_geo`
-#' 
+#'
 #' @return A data frame object containing the summary statistics of columns `dap`, `g`,
-#' `altura`, and `vol_geo`, grouped by the `ut` column. 
-#' 
+#' `altura`, and `vol_geo`, grouped by the `ut` column.
+#'
 #' @import ggplot2
 #' @import dplyr
 #' @import tools
-#' 
+#'
 library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
 
 
-stat_ut <- function(df) {
-    summary_by_ut <- df %>%
+stat_ut <- function(dataframe) {
+    summary_by_ut <- dataframe %>%
     group_by(ut) %>%
     summarize(
         n = n(),
@@ -46,12 +46,12 @@ stat_ut <- function(df) {
         rename_with(~ tools::toTitleCase((gsub("_", " ", .x, fixed = TRUE)))) %>%
         rename(ut = Ut) %>%
         arrange(ut)
-    
+
     output_dir <- './output/Planilhas/'
     if (!dir.exists(output_dir)) {
         dir.create(output_dir, output_dir, recursive = TRUE)
     }
-    
+
     write.csv2(summary_by_ut,
                paste0(output_dir, 'estatistica_por_ut.csv'),
                row.names = FALSE,
