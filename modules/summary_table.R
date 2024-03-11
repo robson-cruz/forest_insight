30e575fc25c763130839ba00870ebfbd15fcb19c
rcDeveloping <rcflorestal@yahoo.com.br>
2024-03-11 15:54:20 -0300

    Create summary table - Table 1

--- modules/summary_table.R
+++ modules/summary_table.R
@@ -1,103 +1,121 @@
-#' summary_table function
-#'
+#' @name summary_table
+#' 
+#' @title Generate Summary Table for Forest Inventory Data
+#' 
 #' @description
-#' This function takes any data frame from a global forest inventory and make a
-#' summary table with the data.
+#' This function generates a summary table for forest inventory data including
+#' descriptive statistics for variables such as diameter at breast height (dap),
+#' basal area (g), height (altura), and volume (vol_geo). The summary table is
+#' saved as both a CSV file and a PNG image.
 #'
-#' @param dataset
+#' @param dataframe A dataframe containing forest inventory data with columns
+#'                  for DAP, basal area, height, and volume.
+#' @return NULL
+#' @export
 #'
+#' @import gt
+#' @importFrom htmltools html
+#' @importFrom dplyr everything
+#' @importFrom dplyr mutate
+#' @importFrom stats summary
+#' @importFrom utils write.csv2
+#' 
+#' @examples
+#' # Assuming df is your dataframe containing forest inventory data
+#' summary_table(df)
 #'
-
 summary_table <- function(dataframe) {
-        summaryFI <- dataframe %>%
-                select(dap, g, altura, volume) %>%
-                summary()
+    table_dir <- "output/Tabelas/"
+    spreadsheet_dir <- "output/Planilhas/"
+    if (!dir.exists(table_dir)) dir.create(table_dir)
+    if (!dir.exists(spreadsheet_dir)) dir.create(spreadsheet_dir)
+    
+    summary_data <- data.frame(
+        dap = as.vector(summary(dataframe$dap)),
+        g = as.vector(summary(dataframe$g)),
+        altura = as.vector(summary(dataframe$altura)),
+        vol_geo = as.vector(summary(dataframe$vol_geo)),
+        row.names = c("Mínimo", "1º Quartil", "Mediana", "Média", "3º Quartil", "Máximo")
+    )
 
-        write.csv2(
-                summaryFI,
-                './output/dataFrame/Tabela_1.csv',
-                row.names = FALSE
+    write.csv2(summary_data, "./output/Planilhas/Tabela_1.csv", row.names = TRUE, fileEncoding = "latin1")
+
+    # set table
+    title_table <- paste0("Tabela 1. Estatística descritiva para as principais variáveis do inventário florestal da UPA.")
+    subtitle_table <- paste0("Em que: DAP (cm) = Diâmetro a altura do peito calculado em cm; Altura (m) = Altura comercial em metros estimada durante o inventário florestal; g (m²) = Área basal calculada em metros quadrados; Volume (m³)= Volume calculado a partir de equação ajustada para a UMF, unidade em metros cúbicos.")
+
+    summary_table <- summary_data |>
+        gt(rownames_to_stub = TRUE) |>
+        cols_label(
+            dap = md("**DAP (cm)**"),
+            g = md("**g (m2)**"),
+            altura = md("**Altura (m)**"),
+            vol_geo = md("**Volume (m3)**")
+        ) |>
+        tab_header(
+            title = html(
+                paste("<p style='text-align:justify'><i>", title_table, "</i></p>")
+            ),
+            subtitle = html(
+                paste("<p style='text-align:justify'><i>", subtitle_table, "</i></p>")
+            )
+        ) |>
+        tab_style(
+            style = cell_text(style = "italic"),
+            locations = list(cells_stubhead(), cells_stub())
+        ) |>
+        cols_align(align = "center") |>
+        tab_style(
+            style = list(cell_borders(sides = c("top", "bottom"),
+                                      color = "#fff", weight = px(1)),
+                         cell_text(align = "center"),
+                         cell_fill(color = "#fff", alpha = NULL)),
+            locations = cells_body(columns = everything(), rows = everything())
+        ) |>
+        tab_style(
+            style = cell_borders(color = "#fff"),
+            locations = cells_stub()
+        ) |>
+        tab_source_note(
+            source_note = html(
+                "<div align='center'><i>",
+                paste0("Fonte: Inventário Florestal da UPA."),
+                "</i></div>"
+            )
+        ) |>
+        tab_style(
+            style = cell_borders(color = "#fff"),
+            locations = cells_stub()
+        ) |>
+        tab_options(# make the title size match the body text
+            #heading.title.font.size = px(14),
+            table.font.size = px(12),
+            table.width = pct(100)
+        ) |>
+        tab_options(
+            heading.border.bottom.width = 2,
+            heading.border.bottom.color = "#000000",
+            # hide the top-most border
+            table.border.top.color = "#fff",
+            # change the column labels section
+            column_labels.border.top.width = 2,
+            column_labels.border.top.color = "#000000",
+            column_labels.border.bottom.width = 2,
+            column_labels.border.bottom.color = "#000000",
+            # Change the vertical lines of summary body
+            stub.background.color = "#fff",
+            # Change the foot source body
+            footnotes.border.bottom.color = "#fff",
+            # change the bottom of the body
+            table_body.border.bottom.color = "#000000",
+            table_body.border.bottom.width = 2,
+            # hide the bottom-most line or footnotes
+            # will have a border
+            table.border.bottom.color = "#fff",
+            # make the width 100%
+            table.width = pct(100),
         )
 
-        tab <- read.csv2('./output/dataFrame/Tabela_1.csv')
-
-        # set table
-        title_table <- paste0('Tabela 1. Estatística descritiva para as variáveis do inventário florestal da UPA ', upa, ', UMF ', umf, ', FLONA ', flona,'.<br> Em que: DAP (cm) = Diâmetro a altura do peito calculado em cm; Altura (m) = Altura comercial em metros estimada durante o inventário florestal; g (m²) = Área basal calculada em metros quadrados; Volume (m³)= Volume calculado a partir de equação ajustada para a UMF ', umf, ' unidade em metros cúbicos.')
-
-        summary_table <- tab %>%
-                gt() %>%
-                cols_label(
-                        X.....dap = md('**DAP (cm)**'),
-                        X......g = md('**g (m2)**'),
-                        X....altura = md('**Altura (m)**'),
-                        X....volume = md('**Volume (m3)**')
-                ) %>%
-                tab_header(
-                        title = html(paste('<p style="text-align:center"><i>',title_table,'</i></p>'))
-                ) %>%
-                tab_style(
-                        style = list(
-                                cell_borders(
-                                        sides = c('top', 'bottom'),
-                                        color = 'white',
-                                        weight = px(1)
-                                ),
-                                cell_text(
-                                        align = 'center'
-                                ),
-                                cell_fill(color = 'white', alpha = NULL)
-                        ),
-                        locations =
-                                cells_body(
-                                        columns = everything(),
-                                        rows = everything()
-                                )
-                ) %>%
-                tab_style(
-                        style = cell_borders(
-                                color = 'white',
-                        ),
-                        locations = cells_stub()
-                ) %>%
-                tab_source_note(
-                        source_note = html('<div align="center"><i>',
-                                           paste0('Fonte: Inventário Florestal UPA ',
-                                                  upa, ', UMF ', umf, ', FLONA ', flona, '.'),
-                                           '</i></div>')
-                ) %>%
-                tab_style(
-                        style = cell_borders(
-                                color = 'white',
-                        ),
-                        locations = cells_stub()
-                ) %>%
-                tab_options(## make the title size match the body text
-                        heading.title.font.size = px(16),
-                        table.width = pct(100)
-                ) %>%
-                tab_options(
-                        ## hide the top-most border
-                        table.border.top.color = 'white',
-                        ## change the column labels section
-                        column_labels.border.top.width = 3,
-                        column_labels.border.top.color = 'black',
-                        column_labels.border.bottom.width = 3,
-                        column_labels.border.bottom.color = 'black',
-                        ## Change the vertical lines of summary body
-                        stub.border.color = 'white',
-                        stub.background.color = 'white',
-                        ## Change the foot source body
-                        footnotes.border.bottom.color = 'white',
-                        ## change the bottom of the body
-                        table_body.border.bottom.color = 'black',
-                        ## hide the bottom-most line or footnotes
-                        ## will have a border
-                        table.border.bottom.color = 'white',
-                        ## make the width 100%
-                        table.width = pct(80),
-                        table.background.color = 'white'
-                )
-
-        ## Save the Table 1 in .png file format
-        gt::gtsave(summary_table, filename = 'Tabela_1.png', path = './output/table')
+    # Save the Table 1 in .png file format
+    gt::gtsave(summary_table, filename = 'Tabela_1.png', path = './output/Tabelas/')
 }
