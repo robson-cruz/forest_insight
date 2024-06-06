321608280d4c328c2dc39e178038fc8d350cc7a3
Robson Cruz <rcflorestal@yahoo.com.br>
2024-06-06 20:06:45 -0300

    Add occurrence_map_specie function to Generate Maps for Species Occurrences

--- modules/scientific_name_clean.R
+++ modules/scientific_name_clean.R
@@ -4,11 +4,18 @@
 #'
 #' @description
 #' This function takes a data frame from a global forest inventory and points
-#' out typos and taxonomic synonym by using the globalnames API from
-#' http://verifier.globalnames.org/data_sources
+#' out typos by using the globalnames API from
+#' http://verifier.globalnames.org/data_sources. Identifies taxonomic synonyms and
+#' geographical occurrence of species using the REFLORA database.
 #'
 #' @param dataframe
 #'
+#'#'@references
+#' For information about the REFLORA database, see:
+#' \url{https://reflora.jbrj.gov.br/reflora/listaBrasil/ConsultaPublicaUC/ResultadoDaConsultaNovaConsulta.do#CondicaoTaxonCP}
+#' For information about the globalnames API, see:
+#' \url{https://verifier.globalnames.org/api}
+#' 
 #'@import httr
 #'@import jsonlite
 #'@import magrittr
@@ -26,6 +33,34 @@ library(dplyr, warn.conflicts = FALSE)
 
 
 scientific_name_clean <- function(dataframe) {
+    # Basic data cleaning
+    dataframe %<>%
+        # remove extra white space between genus and epithet
+        mutate(
+            nome_cientifico = str_squish(nome_cientifico),
+            
+            # remove white space from the beginning and end of scientific names
+            nome_cientifico = str_trim(nome_cientifico),
+            
+            # Capitalization inconsistency
+            nome_cientifico = tolower(nome_cientifico),
+            nome_cientifico = R.utils::capitalize(nome_cientifico),
+            
+            # get only the scientific name, without taxonomist's names
+            nome_cientifico = str_extract(
+                nome_cientifico,
+            "(\\w+\\s\\w+)(-\\w+)?(\\s\\w+\\ssubsp.\\s\\w+)?(\\ssubsp.\\s\\w+)?(\\svar.\\s\\w+)?(\\.\\s\\w+)?"
+            ),
+            # Remove sp, sp., spp and spp.
+            nome_cientifico = gsub("(\\w+)\\sspp$|\\ssp$||\\ssp.$|\\sspp.$", "\\1", nome_cientifico),
+            # Remove typos due SISTAXON list
+            nome_cientifico = recode(
+                nome_cientifico,
+                "Handroanthus impetiginosum" = "Handroanthus impetiginosus",
+                
+            )
+        )
+    
     # Get unique scientific names form the input data set
     input_names <- unique(dataframe$nome_cientifico)
     # Get data set column names
@@ -35,40 +70,38 @@ scientific_name_clean <- function(dataframe) {
     # Brazilian Ministry of Environment (Portaria MMA 443/2014) and Resolution 54/2007
     # from the Pará state Brazil
     port <- readr::read_csv2(
-        'https://raw.githubusercontent.com/robson-cruz/endangeredBrazilianPlantSpecies/main/output/Especies_Ameacadas_BRA.csv',
+        "https://raw.githubusercontent.com/robson-cruz/endangeredBrazilianPlantSpecies/main/output/Especies_Ameacadas_BRA.csv",
             locale = readr::locale(encoding = "latin1")
     ) %>%
-        filter(lista %in% c('Lista Flora Ameaçada Pará', 'Lista Portaria 443')) %>%
-        select(-c(1, 4, 5, 6, 8)) %>%
-        group_by(nome_cientifico, status_conservacao) %>%
+        filter(lista %in% c("Lista Flora Ameaçada Pará", "Lista Portaria 443")) |>
+        group_by(nome_cientifico, status_conservacao) |>
         summarise(dispositivo_legal = paste(unique(dispositivo_legal), collapse = ", "))
 
     # Read "reflora" data set to get specie geographic distribution
-    # reflora <- readr::read_csv2(
-    #     'https://raw.githubusercontent.com/robson-cruz/endangeredBrazilianPlantSpecies/main/data/reflora20230606.csv'
-    # ) %>%
-    #     select(specie, locationID) %>%
-    #     rename(specieReflora = specie)
-
-    # Basic data cleaning
-    dataframe %<>%
-        # remove extra white space between genus and epithet
-        mutate(nome_cientifico = str_squish(nome_cientifico),
-
-               # remove white space from the beginning and end of scientific names
-               nome_cientifico = str_trim(nome_cientifico),
-
-               # Capitalization inconsistency
-               nome_cientifico = tolower(nome_cientifico),
-               nome_cientifico = R.utils::capitalize(nome_cientifico),
-
-               # get only the scientific name, without taxonomist's names
-               nome_cientifico = str_extract(
-                       nome_cientifico,
-                       '(\\w+\\s\\w+)(-\\w+)?(\\s\\w+\\ssubsp.\\s\\w+)?(\\ssubsp.\\s\\w+)?(\\svar.\\s\\w+)?(\\.\\s\\w+)?'
-               )
-        )
-
+    reflora <- readr::read_csv2(
+        "https://raw.githubusercontent.com/robson-cruz/endangeredBrazilianPlantSpecies/main/data/reflora_v393_400.csv"
+    )
+    
+    reflora_location <- readr::read_csv2(
+        "https://raw.githubusercontent.com/robson-cruz/endangeredBrazilianPlantSpecies/main/data/reflora_distribuicao_v393_400.csv"
+    )
+    
+    reflora_location_by_state <- reflora_location |>
+        inner_join(reflora, by = "id") |>
+        group_by(specie) |>
+        summarize(state = paste(locationID, collapse = ", "))
+    
+    reflora_not_amz <- reflora_location |>
+        mutate(
+            occurrenceRemarks = gsub(
+                ",", ", ", gsub("\\[|\\]", "",
+                                str_extract(occurrenceRemarks, "\\[([A-Za-zà-ÿ, ]*)\\]"))
+            )
+        ) |>
+        mutate(amz_biom = str_extract(occurrenceRemarks, "Amazônia")) |>
+        inner_join(reflora, by = "id") |>
+        filter(is.na(amz_biom), !is.na(occurrenceRemarks))
+    
     # Using POST method in globalnames API
     post_global_names <- httr::POST(
         "https://verifier.globalnames.org/api/v1/verifications",
@@ -76,35 +109,52 @@ scientific_name_clean <- function(dataframe) {
                                    `Content-Type` = "application/json"),
         body = jsonlite::toJSON(list(nameStrings = input_names)
         ))
-
-    data <- jsonlite::fromJSON(rawToChar(post_global_names$content))$names %>%
-        rename(submitedName = name) %>%
+    
+    
+    data <- jsonlite::fromJSON(rawToChar(post_global_names$content))$names |>
+        rename(submitedName = name) |>
         mutate(
             typo = if_else(matchType == "Fuzzy", TRUE, FALSE)
-        ) %>%
-        select(-c(matchType, curation)) %>%
-        tidyr::unnest_wider(bestResult) %>%
+        ) |>
+        select(-c(matchType, curation)) |>
+        tidyr::unnest_wider(bestResult) |>
         mutate(
             familia = gsub(".*(\\b\\w+ceae\\b).*", "\\1", classificationPath)
-        ) %>%
-        select(submitedName, typo, isSynonym, currentCanonicalSimple, familia)
+        )
 
     # Join data
     dataframe %<>%
-        tibble::rowid_to_column(var = 'id') %>%
+        tibble::rowid_to_column(var = 'id_tmp') %>%
         left_join(data,
                   by = c("nome_cientifico" = "submitedName"),
                   relationship = "many-to-many") %>%
-        distinct(id, .keep_all = TRUE) %>%
-        select(id, all_of(col_names), typo, isSynonym, currentCanonicalSimple, familia) %>%
-        rename(nome_aceito = currentCanonicalSimple,
-               erro_digitação = typo,
-               sinônimo = isSynonym)
-
+        distinct(id_tmp, .keep_all = TRUE) %>%
+        select(id_tmp, all_of(col_names), typo, familia) %>%
+        rename(erro_digitação = typo)
+    
+    dataframe <- dataframe |>
+        left_join(reflora, by = c("nome_cientifico" = "specie"), relationship = "many-to-many") |>
+        mutate(specie = if_else(is.na(id), "", paste(genus, specificEpithet, sep = " "))) |>
+        distinct(num_arvore, .keep_all = TRUE) |>
+        mutate(specie_clean = if_else(!is.na(acceptedNameUsage), acceptedNameUsage, specie)) |>
+        mutate(ocor_amz = if_else(specie == "" | specie %in% reflora_not_amz$specie, FALSE, TRUE)) |>
+        left_join(reflora_location_by_state, by = c("specie_clean" = "specie"), relationship = "many-to-many") |>
+        mutate(ocor_uf_pa_aux = stringr::str_extract(state, "PA")) |>
+        mutate(ocor_uf_pa = if_else(is.na(ocor_uf_pa_aux), FALSE, TRUE)) |>
+        tidyr::replace_na(
+            list(state = "", dispositivo_legal = "", acceptedNameUsage = "",
+                 specie = "", nomenclaturalStatus = "", taxonomicStatus = "",
+                 genus = "", scientificName = "", family = "", specificEpithet = "")
+        ) |>
+        rename(estado = state, nome_aceito = acceptedNameUsage) |>
+        mutate(familia = if_else(is.na(familia), family, familia)) |>
+        select(-c(ocor_uf_pa_aux, infraspecificEpithet, family))
+    
     dataframe %<>%
-        left_join(port, by = c("nome_aceito" = "nome_cientifico"),
+        left_join(port,
+                  by = c("specie_clean" = "nome_cientifico"),
                   relationship = "many-to-many") %>%
-        distinct(id, .keep_all = TRUE) %>%
+        distinct(id_tmp, .keep_all = TRUE) %>%
         # NA values replaced by "Nao Ameaçada"
         mutate(
             status_conservacao = if_else(is.na(status_conservacao),
@@ -112,15 +162,20 @@ scientific_name_clean <- function(dataframe) {
                                          status_conservacao)
         ) %>%
         # Set status_conservacao column as a factor
-        mutate(
-            status_conservacao = factor(status_conservacao,
-                            levels = c("Não Ameaçada", "Vulnerável", "Em Perigo", "Criticamente em Perigo"))
-        ) %>%
-        select(-c(id))
+        mutate(status_conservacao = factor(
+            status_conservacao,
+            levels = c(
+                "Não Ameaçada",
+                "Vulnerável",
+                "Em Perigo",
+                "Criticamente em Perigo"
+            )
+        )) %>%
+        select(-c(id_tmp, specie_clean))
 
     # Filter typo and save it
-    typo <- dataframe %>%
-        filter(erro_digitação == TRUE) %>%
+    typo <- dataframe |>
+        filter(erro_digitação == TRUE) |>
         select(num_arvore, ut, nome_cientifico, nome_aceito)
 
     output_dir_not <- './output/Pendencias/'
@@ -138,9 +193,9 @@ scientific_name_clean <- function(dataframe) {
     }
 
     # Filter taxonomic synonyms and save it
-    synonyms <- dataframe %>%
-        filter(sinônimo == TRUE) %>%
-        select(nome_cientifico, nome_aceito) %>%
+    synonyms <- dataframe |>
+        filter(taxonomicStatus == "SINONIMO") |>
+        select(nome_cientifico, nome_aceito) |>
         unique()
 
     if (nrow(synonyms > 0)) {
@@ -152,7 +207,7 @@ scientific_name_clean <- function(dataframe) {
         )
     }
     
-    # Save forest inventory
+    # Save the forest inventory
     output_dir <- "./output/"
     if (!dir.exists(output_dir)) dir.create(output_dir)
     
