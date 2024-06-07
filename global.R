b251e867243ec0ea2122e88d962c3366b86a2eff
Robson Cruz <rcflorestal@yahoo.com.br>
2024-06-06 21:54:57 -0300

    Arrange columns

--- global.R
+++ global.R
@@ -20,7 +20,11 @@ usr_data <- NULL
 
 setUploadedData <- function(data) {
     usr_data <<- data
-
+    
+    col_names <- colnames(usr_data)
+    
+    col_names_df <- data.frame(index_col = 1:length(col_names), col_name = col_names)
+    
     usr_data %<>%
         mutate(categoria = R.utils::capitalize(categoria)) %>%
         # Diameter at Breast Height - DBH ("dap" in Portuguese)
@@ -33,7 +37,11 @@ setUploadedData <- function(data) {
         mutate(qf = factor(qf)) %>%
         # Define 'Categoria2' column
         mutate(categoria2 = if_else(categoria == "Substituta", "Remanescente", categoria)) %>%
-        mutate(categoria2 = factor(categoria2))
+        mutate(categoria2 = factor(categoria2)) %>%
+        # Arrange columns
+        select(c(1:col_names_df[which(col_names_df$col_name == "volume"), 1],
+                 col_names_df[which(col_names_df$col_name == "vol_geo"), 1],
+                 (col_names_df[which(col_names_df$col_name == "vol_geo"), 1] + 1):length(col_names)))
 
     assign("usr_data", usr_data, inherits = TRUE, envir = .GlobalEnv)
 }
