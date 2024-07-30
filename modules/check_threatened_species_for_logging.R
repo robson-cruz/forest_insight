dee4a3df6909cf85abb6521c8e026a07112bd41b
rcDeveloping <rcflorestal@yahoo.com.br>
2024-07-30 11:44:41 -0300

    Fixed issue #53

--- modules/check_threatened_species_for_logging.R
+++ modules/check_threatened_species_for_logging.R
@@ -36,18 +36,17 @@
 #' @keywords threatened species conservation logging
 check_threatened_species_for_logging <- function(dataframe) {
     
-    check_1 <- dataframe$categoria2 == "Explorar"
-    check_2 <- dataframe$nome_cientifico == "Bertholletia excelsa"
-    check_3 <- dataframe$nome_aceito == "Bertholletia excelsa"
-    check_4 <- dataframe$status_conservacao == "Em Perigo"
+    decreto5975 <- c("Bertholletia excelsa", "Hevea Brasiliensis")
     
-    df <- dataframe[check_1 & check_2 | check_3 | check_4, ]
-    
-    df_output <- df[df$categoria2 == "Explorar", ]
-    df_output <- df_output |> tidyr::drop_na()
+    df_output <- dataframe %>%
+        filter(
+            categoria2 == "Explorar" &
+                (nome_cientifico %in% decreto5975 | nome_aceito %in% decreto5975 |
+                     status_conservacao == "Em Perigo")
+        )
     
     if (nrow(df_output) > 0) {
-        issue_dir <- './output/Pendencias/'
+        issue_dir <- "./output/Pendencias/"
         if (!dir.exists(issue_dir)) {
             dir.create(issue_dir, recursive = TRUE)
         }
@@ -59,16 +58,12 @@ check_threatened_species_for_logging <- function(dataframe) {
     }
     
     check_typo <- function() {
-        
-        typo <- df_output[df_output$nome_cientifico != df_output$nome_aceito, ]
-        
+        typo <- df_output[df_output$nome_aceito != "", ]
         if (nrow(typo) > 0) {
             species_typo <<- unique(typo$nome_cientifico)
             accepted_name <<- unique(typo$nome_aceito)
-            
             return(TRUE)
-        }
-        else {
+        } else {
             return(FALSE)
         }
     }
@@ -80,59 +75,58 @@ check_threatened_species_for_logging <- function(dataframe) {
         )
     }
     
-    if (nrow(df_output) > 0 & length(unique(df_output$nome_aceito)) == 1) {
-        if (check_typo()) {
-            output_text <<- paste0(
-                "No inventário florestal constam árvores selecionadas para corte da espécie ",
-                glue::glue_collapse(unique(df_output$nome_aceito), last = " e "),
-                ", no entanto, esta espécie é proíbida de corte. Registra-se nesta análise que há erro de digitação, a saber: ",
-                unique(accepted_name),
-                " foi grafada de forma incorreta como ",
-                "\"", unique(species_typo), "\"."
-            )
-            save_text(output_text)
-        } 
-        else {
-            output_text <<- paste0(
-                "No inventário florestal constam árvores selecionadas para corte da espécie ",
-                unique(df_output$nome_cientifico)
-            )
-            save_text(output_text)
-        }
-    
-    }
-    else if (nrow(df_output) > 0 & length(unique(df_output$nome_aceito)) > 1) {
-        if (check_typo()) {
-            if (length(species_typo) == 1) {
-                output_text <<- paste0(
-                    "No inventário florestal constam árvores selecionadas para corte das espécies ",
-                    glue::glue_collapse(df_output$nome_aceito, sep = ", ", last = " e "),
-                    ", no entanto, estas espécies são proíbidas de corte. Registra-se nesta análise que há erro de digitação no inventário, a saber: ",
-                    accepted_name,
+    if (nrow(df_output) > 0) {
+        if (length(unique(df_output$nome_aceito)) == 1) {
+            if (check_typo()) {
+                output_text <- paste0(
+                    "No inventário florestal constam árvores selecionadas para corte da espécie ",
+                    glue::glue_collapse(unique(df_output$nome_aceito), last = " e "),
+                    ", no entanto, esta espécie é proibida de corte por estar Em Perigo de Extinção. Registra-se nesta análise que há erro de digitação, a saber: ",
+                    unique(accepted_name),
                     " foi grafada de forma incorreta como ",
-                    "\"", species_typo, "\"."
+                    "\"", unique(species_typo), "\"."
+                )
+                save_text(output_text)
+            } else {
+                output_text <- paste0(
+                    "No inventário florestal constam árvores selecionadas para corte da espécie ",
+                    unique(df_output$nome_cientifico),
+                    ", no entanto, esta espécie é proibida de corte por estar Em Perigo de Extinção."
                 )
                 save_text(output_text)
             }
-            else {
-                output_text <<- paste0(
+        } else if (length(unique(df_output$nome_aceito)) > 1) {
+            if (check_typo()) {
+                if (length(species_typo) == 1) {
+                    output_text <- paste0(
+                        "No inventário florestal constam árvores selecionadas para corte das espécies ",
+                        glue::glue_collapse(df_output$nome_cientifico, sep = ", ", last = " e "),
+                        ", no entanto, estas espécies são proibidas de corte por estarem Em Perigo de Extinção. Registra-se nesta análise que há erro de digitação no inventário, a saber: ",
+                        accepted_name,
+                        " foi grafada de forma incorreta como ",
+                        "\"", species_typo, "\"."
+                    )
+                    save_text(output_text)
+                } else {
+                    output_text <- paste0(
+                        "No inventário florestal constam árvores selecionadas para corte das espécies ",
+                        glue::glue_collapse(unique(df_output$nome_aceito), sep = ", ", last = " e "),
+                        glue::glue_collapse(unique(df_output$nome_cientifico), sep = ", ", last = " e "),
+                        ", no entanto, estas espécies são proibidas de corte por estarem Em Perigo de Extinção. Registra-se nesta análise que há erro de digitação no inventário, a saber: ",
+                        glue::glue_collapse(unique(accepted_name), last = " e "),
+                        " foram grafadas de forma incorreta como ",
+                        "\"", glue::glue_collapse(unique(species_typo), last = " e "), "\"."
+                    )
+                    save_text(output_text)
+                }
+            } else {
+                output_text <- paste0(
                     "No inventário florestal constam árvores selecionadas para corte das espécies ",
-                    glue::glue_collapse(unique(df_output$nome_aceito), sep = ", ", last = " e "),
-                    ", no entanto, estas espécies são proíbidas de corte. Registra-se nesta análise que há erro de digitação no inventário, a saber: ",
-                    glue::glue_collapse(unique(accepted_name), last = " e "),
-                    " foram grafadas de forma incorreta como ",
-                    "\"", glue::glue_collapse(unique(species_typo), last = " e "), "\"."
+                    glue::glue_collapse(unique(df_output$nome_cientifico), sep = ", ", last = " e "),
+                    ", no entanto, estas espécies são proibidas de corte por estarem Em Perigo de Extinção."
                 )
                 save_text(output_text)
             }
-        }
-        else {
-            output_text <<- paste0(
-                "No inventário florestal constam árvores selecionadas para corte das espécies ",
-                glue::glue_collapse(unique(df_output$nome_cientifico), sep = ", ", last = " e "),
-                ", no entanto, estas espécies são proíbidas de corte."
-            )
-            save_text(output_text)
         }
     }
 }
